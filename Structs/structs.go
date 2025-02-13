648b82551068808bff137c986261a421d0713516
Yuvraj Shekhar <syuvraj1999@gmail.com>
2025-02-13 16:13:12 +0100

    added constructors and used mutation methods in structs

--- Structs/structs.go
+++ Structs/structs.go
@@ -1,22 +1,63 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"time"
 )
 
+type user struct {
+	firstName string
+	lastName  string
+	birthdate string
+	createdAt time.Time
+}
+
+func (u *user) clearUsername() {
+	u.firstName = ""
+	u.lastName = ""
+}
+
+func (u user) outputUserDetails() {
+	fmt.Println(u.firstName, u.lastName, u.birthdate)
+}
+
+func newUser(firstName, lastName, birthdate string) (*user, error) {
+	if firstName == "" || lastName == "" || birthdate == "" {
+		return nil, errors.New("First Name, Last name or birthdate not found")
+	}
+	return &user{
+		firstName: firstName,
+		lastName:  lastName,
+		birthdate: birthdate,
+	}, nil
+}
+
 func main() {
-	firstName := getUserData("Please enter your first name: ")
-	lastName := getUserData("Please enter your last name: ")
-	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+	user_firstName := getUserData("Please enter your first name: ")
+	user_lastName := getUserData("Please enter your last name: ")
+	user_birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	var appUser *user
+	appUser, err := newUser(user_firstName, user_lastName, user_birthdate)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	// ... do something awesome with that gathered data!
+	appUser.outputUserDetails()
+	appUser.clearUsername()
+	appUser.outputUserDetails()
+}
 
-	fmt.Println(firstName, lastName, birthdate)
+func outputUserDetails(u *user) {
+	fmt.Println(u.firstName, u.lastName, u.birthdate)
 }
 
 func getUserData(promptText string) string {
 	fmt.Print(promptText)
 	var value string
-	fmt.Scan(&value)
+	fmt.Scanln(&value)
 	return value
 }
